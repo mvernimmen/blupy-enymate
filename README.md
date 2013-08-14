07ab822c2657e47aa1a1a644881ef742cf67f833
Max Vernimmen <m.vernimmen@gmail.com>
2013-08-14 20:59:22 +0200

    Added an analyser function that takes the received data and starts analysing it. this will be the basis for interperting the data and then feeding it to mysql or other output system.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ What is it? A python script to read the enymate sensor device through bluetooth.
 Purpose: get data from enymate device and store it into a mysql database for
  later processing and visualisation. A python learning project for its creater.
 
-# details:
+## Details:
 The enymate is a device that was sold in the Netherlands in 2009. It allows up
 to 3 sensors to be connected to it which can read electricity, gas and water
 usage using optical sensors. The creater of enymate provides windows and mac
@@ -16,8 +16,8 @@ I'm developing this python program on a raspberry pi, which I've added a
 bluetooth usb device to. This is one of my very first python scripts.
 Feedback is appreciated.
 
-# Installation:
-get python 2.7, install bluetooth drivers, software, etc
+## Installation:
+Get python 2.7, install bluetooth drivers, software, etc
 edit this script and fill in the mac address of your enymate (you can find it
 with bluetooth scanning tools). pair your enymate with the machine you intend
 to run this script on. Then run this script.
@@ -25,3 +25,10 @@ to run this script on. Then run this script.
 Currently (20130813) it receives only realtime data from the enymate but it doesn't
 yet interpret it nor does it store anything yet.
 
+## Troubleshooting:
+If you get 'no route to host', restart your bluetooth stack:
+/etc/init.d/bluetooth restart
+When you get 'couln't connect to device' then just wait a few minutes.
+The enymate is still trying to send data over the previous connection and hasn't
+realised there is a new connection. Unfortunately it can only make one connection
+at a time
--- reader.py
+++ reader.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python
+
+# Program written by Max Vernimmen
+# This connects via bluetooth to the enymate reader and sends a 'read'
+# command. After that it keeps on receiving data and printing it
+# until you kill it.
+
 import serial
 import string
 import time
@@ -19,33 +26,85 @@ def connect():
             time.sleep(10)
     return gaugeSocket;
 
+def analyse(receivedData):
+	#so we've received some data... what does it mean?
+	#als de eerste 3 bytes x00 x0f x00 zijn dan is het een meter uitput
+	#byte 4: sensor nr
+	#bytes 5-9 data
+	#byte 10: altijd x41
+	#byte 11: status byte. onbekend wat we daar mee moeten.
+
+	print " Entering analyse"
+	#for x in receivedData:
+	#	print x
+
+	#if receivedData[0]:
+	#	if receivedData[0] == b'\x00':
+	#		print "1st byte was 0x00!"
+	#	else:
+	#		print "1st byte not recognised as 0x00: %s" % receivedData[0].encode('hex')
+	#if receivedData[1]:
+	#	if receivedData[1] == b'\x0f':
+	#		print "1st byte was 0x0f!"
+	#	else:
+	#		print "1st byte not recognised as 0x0f: %s" % receivedData[0].encode('hex')
+	if (receivedData[0] == b'\x00') and (receivedData[1] == b'\x0f') and (receivedData[2] == b'\x00') and ( (receivedData[9] == b'\x40') or (receivedData[9] == b'\x41') ) :
+		type="ok"
+		#probably need to add a base value to get the ascii representation of 1-4 here
+	else:
+		print "analysis says this was an invalid message"
+		return
+	
+	if (type=="ok"):
+		print "sensor number is: %s" % receivedData[3].encode('hex')
+		if ( b'\x41' <= receivedData[3] <= b'\x45' ):
+			print "this is a water sensor"
+		elif ( b'\x81' <= receivedData[3] <= b'\x89' ):
+			print "this is a gas sensor"
+		elif ( b'\x01' <= receivedData[3] <= b'\xff' ):
+			print "this is an electricity sensor"
+
+
+
+	#when trailing byte is 40h instead of 41h, then the info is the 'timeout' info for where there were no measurements for that sensor
+	print ""
+
+
+
+## --------------------------- main program start -----------------------------
+
+
 gaugeSocket = connect()
 
 #make sure we close the connection when we get killed
 import signal
 import sys
-#def signal_handler(signal, frame):
-#        print 'You pressed Ctrl+C!'
-#	if gaugeSocket:
-#		print ' sending termination to enymate'
-#		gaugeSocket.send(str("\xF6"))
-#		gaugeSocket.close()
-#        sys.exit(0)
-#signal.signal(signal.SIGINT, signal_handler)
+def signal_handler(signal, frame):
+        print 'You pressed Ctrl+C!'
+	if gaugeSocket:
+		print ' sending termination to enymate'
+		gaugeSocket.send(str("\xF6"))
+		gaugeSocket.close()
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 gaugeSocket.send(str("\xF9"))
-print("The get usage command has been sent: " + str("\xF9"))
+print("The get usage command has been sent: F9h")
 
 while(True):
     try:
         data = gaugeSocket.recv(512)
 	if len(data) == 0: break
-#	if len(data) != 11: data.append(gaugeSocket.recv(512))
+	while(len(data) < 11):
+		data +=gaugeSocket.recv(512)
+		print "waiting for more data"
 	print "[%s] received packet of size %d" % (time.strftime('%Y-%m-%d %H:%M:%S'), len(data))
-	print " in hex: "
+	print " in hex: ",
 	for x in data:
 		print ("%s" % x.encode('hex')),
 	print ""
+	analyse(data)
+
     except bluetooth.btcommon.BluetoothError as error:
         print "Caught BluetoothError: ", error
 	print " trying new connection in 5 seconds"
@@ -58,3 +117,4 @@ while(True):
 	gaugeSocket.close()
 
 gaugeSocket.close()
+
